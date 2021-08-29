48848b90edaf344de8edcacd3f6047d73ba35e11
Sylas Serné <its.sylas@gmail.com>
2021-08-29 13:51:12 -0700

    seed typos removed

--- express-back-end/db/seeds/03_games.sql
+++ express-back-end/db/seeds/03_games.sql
@@ -1,6 +1,5 @@
 INSERT INTO games (title, description, platform)
-VALUES
-
+VALUES 
 ('Scrabble Extreme', 'Creating words on a board under threat of a hired gunman', 'PS5'),
-("Bowser's contemplation", "a harrowing meditation on evil", "Switch"),
-("The Legend of Zelda Bad Breath of the Wild", "Finally, brush ganondorf's teeth!", "Switch");
+('Bowser''s contemplation', 'a harrowing meditation on evil', 'Switch'),
+('The Legend of Zelda Bad Breath of the Wild', 'Finally, brush ganondorf''s teeth!', 'Switch');
--- express-back-end/db/seeds/04_review_likes.sql
+++ express-back-end/db/seeds/04_review_likes.sql
@@ -1,8 +1,8 @@
 INSERT INTO likes (user_id, review_id, type)
 VALUES 
 
-(1, 3, "like"),
-(1, 2, "hmm"),
-(1, 1, "haha"),
-(2, 3, "like"),
-(2, 2, "haha");
+(1, 3, 'like'),
+(1, 2, 'hmm'),
+(1, 1, 'haha'),
+(2, 3, 'like'),
+(2, 2, 'haha');
