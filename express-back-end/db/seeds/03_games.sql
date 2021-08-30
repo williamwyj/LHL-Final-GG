9a778edb133e79bf514c729e28ab310276ab00cf
William Wang <williamwyj1@gmail.com>
2021-08-29 23:07:18 -0700

    add 4 games data to home page

--- express-back-end/db/seeds/03_games.sql
+++ express-back-end/db/seeds/03_games.sql
@@ -1,5 +1,6 @@
-INSERT INTO games (title, description, platform)
+INSERT INTO games (title, description, platform, cover)
 VALUES 
-('Scrabble Extreme', 'Creating words on a board under threat of a hired gunman', 'PS5'),
-('Bowser''s contemplation', 'a harrowing meditation on evil', 'Switch'),
-('The Legend of Zelda Bad Breath of the Wild', 'Finally, brush ganondorf''s teeth!', 'Switch');
+('Scrabble Extreme', 'Creating words on a board under threat of a hired gunman', 'PS5', '/image/game_cover/scrabble_extreme.jpeg'),
+('Bowser''s Fury', 'a harrowing meditation on evil', 'Switch', '/image/game_cover/bowsers_fury.jpeg'),
+('The Legend of Zelda Bad Breath of the Wild', 'Finally, brush ganondorf''s teeth!', 'Switch', '/image/game_cover/legend_of_zelda_breath_of_the_wild.jpeg'),
+('Metroid: Samus Returns', 'Metroid: Samus Returns is a remake of Metroid 2: Return of Samus.', '3DS', '/image/game_cover/metroid_samus_returns.jpeg');
--- react-front-end/src/components/home.js
+++ react-front-end/src/components/home.js
@@ -1,36 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 import "./home.scss"
 
-class Home extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+import GameBox from "./HomePage/GameBox/GameBox"
 
-  fetchData = () => {
-    axios.get('/api/games') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
-      this.setState({
-        message: response.rows
-      });
-    }) 
-  }
+import useApplicationData from './hooks/useApplicationData';
 
-  render() {
-    return (
-      <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data Home Page
-        </button>        
-      </div>
-    );
-  }
+export default function Home() {
+  const { state } = useApplicationData();
+  // const fetchData = () => {
+  //   axios.get('/api/games') // You can simply make your requests to "/api/whatever you want"
+  //   .then((response) => {
+  //     // handle success
+  //     console.log(response.data) // The entire response from the Rails API
+  //   }) 
+  // }
+
+  
+  return (
+    <div className="App">
+      <h1>Home Page</h1>
+      <button>
+        Fetch Data Home Page
+      </button>    
+      <section className="topGames">
+        {state.games.map((game) => {
+          console.log(game);
+        return <GameBox key={game.id} id={game.id} title={game.title} description={game.description} platform={game.platform} cover={game.cover} />
+        })}
+      </section> 
+      <section className="topReviews">
+
+      </section>   
+    </div>
+  );
+  
 }
 
-export default Home;
