387c732eb87cc550757a212df41c7aab6fbb3b39
Sylas Serné <its.sylas@gmail.com>
2021-09-02 18:04:06 -0700

    clicking a result now leads you to the correct game page, platforms have spaces between them

--- react-front-end/src/components/SearchPage/ResultItem.js
+++ react-front-end/src/components/SearchPage/ResultItem.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { NavDropdown, Navbar } from "react-bootstrap";
-
-import { searchGame } from "../../helpers/dbHelpers";
+import { NavDropdown, Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function ResultItem(props) {
 
+  console.log('RESULT ITEM PLATFORMS:', props.platforms)
+
+  const putInPlace = function(array){
+    return array.map(value => `${value} `)
+  }
+
   return (
-    <section className="gameResult">
+    <nav className="gameResult">
+      <Nav.Link href={`/game/${props.id}`}>
       <img 
       className="cover"
       src={props.cover}
@@ -14,9 +20,9 @@ export default function ResultItem(props) {
       <article>
         <title>{props.name}</title>
         {props.summary}
-        <footer>{props.platforms}</footer>
+        <footer>{putInPlace(props.platforms)}</footer>
       </article>
-      
-    </section> 
+      </Nav.Link>
+    </nav> 
   )
 }
