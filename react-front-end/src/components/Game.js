43f245b2c5c6e726f7e04b7573c4a29bc91c4bed
Sylas Serné <its.sylas@gmail.com>
2021-09-03 14:19:04 -0700

    initial HTML skeleton created

--- react-front-end/src/components/Game.js
+++ react-front-end/src/components/Game.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import "./Game.scss"
 import { useParams } from 'react-router-dom';
 import { grabGameById } from '../helpers/dbHelpers';
+import { Button, Carousel } from 'react-bootstrap'
 
 export default function Game(props) {
   const [game, setGame] = useState("")
@@ -16,10 +17,31 @@ export default function Game(props) {
     });
   }, []);
 
+  // const screenshots = game.screenshots.map((img) =>
+  //   <Carousel.Item interval={1000}>
+  //     <img className="slideshow-image "src={game.screenshots[0]}/>
+  //   </Carousel.Item>
+  // )
+
   return (
-    <div>
-      <h1>Game ID is { id }</h1>     
-      <h2>Game name is { game.name }</h2>
+    <div className="main-container">
+      <div className="game-info">
+        <div className="imgGallery">
+          <img src={game.cover}/>
+        </div>
+        <div className="game-details">
+          <div className="top">
+            <h2 >{ game.name }</h2>
+            <span>{ game.summary }</span>
+          </div>
+          <div className="bottom">
+            <Button> submit review </Button> 
+          </div>
+        </div>
+        <div className = "user-game-interactions">
+          buttons to like/review/follow the GAME
+        </div>
+      </div>
     </div>
   )
 }
