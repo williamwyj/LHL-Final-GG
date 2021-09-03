4683900d9475cf51c7dcfd97bc3e0d0f8a2b832a
Sylas Serné <its.sylas@gmail.com>
2021-09-03 12:16:28 -0700

    route

--- express-back-end/routes/routes.js
+++ express-back-end/routes/routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/games", (req, res) => {
-    db.query("SELECT * FROM games LIMIT 10;")
+    db.query("SELECT * FROM games;")
       .then((data => {
         res.json(data.rows);
       }))
@@ -78,50 +78,6 @@ module.exports = (db) => {
       });
   })
 
-
-  //get userID from username to be used in profile page
-  router.get('/userId', (req,res)=>{
-    const username = req.query.username
-    console.log("username is ", username)
-    db.query(`SELECT users.id FROM users WHERE users.username = '${username}';`)
-      .then((data => {
-        res.json(data.rows);
-      }))
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  })
-
-  //route to get users information with input userId
-  router.get('/user', (req,res) => {
-    const userId = req.query.userId
-    db.query(
-      `SELECT * FROM users WHERE id=${userId};
-      `)
-      .then((data => {
-        res.json(data.rows);
-      }))
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  })
-
-  //route to get number of reviews for a user with input useId
-  router.get('/user/reviewStats', (req, res) => {
-    const userId = req.query.userId
-    db.query(
-      `SELECT users.id,
-      COUNT(*) FILTER (WHERE ${userId} = reviews.user_id) AS reviews
-      FROM users
-      JOIN reviews ON users.id = reviews.user_id
-      WHERE users.id = ${userId}
-      GROUP BY users.id;
-      `)
-
   //search
   //easy refactor later on
   router.get('/search', (req, res) => {
@@ -135,124 +91,19 @@ module.exports = (db) => {
     });
   })
   
-
-  //user_game_relationship stats based on games, liked, played, play_list
-  router.get('/gameuserstats', (req,res)=> {
-    const gameId = req.query.gameId
-    db.query(`
-      SELECT 
-      game_id AS id,
-      COUNT(*) FILTER (WHERE liked = 'TRUE') AS "liked",
-      COUNT(*) FILTER (WHERE played = 'TRUE') AS "played",
-      COUNT(*) FILTER (WHERE play_list = 'TRUE') AS "play_list"
-      FROM user_game_relationships WHERE game_id = ${gameId}
-      GROUP BY game_id;
-    `)
-      .then((data => {
+  //get specific game by Id 
+  router.get("/gameId", (req, res) => {
+    db.query("SELECT * FROM games WHERE id = $1;", [req.query.input])
+      .then((data) => {
+        console.log("ROUTES", data.rows)
         res.json(data.rows);
-      }))
+      })
       .catch(err => {
         res
           .status(500)
           .json({ error: err.message });
       });
   })
-
-  router.get('/user/followStats', (req, res) => {
-    const userId = req.query.userId
-    Promise.all([
-      db.query(
-        `SELECT user_id,
-        COUNT(*) AS followers
-        FROM followers
-        WHERE user_id = ${userId}
-        GROUP BY user_id;
-        `
-      ),
-      db.query(
-        `SELECT follower_id,
-        COUNT(*) AS followed
-        FROM followers
-        WHERE follower_id = ${userId}
-        GROUP BY follower_id;
-        `
-      )
-    ]).then((all => {
-      res.json(all);
-      }))
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-    
-  })
-
-  router.get('/user/favoritegames', (req, res) => {
-    const userId = req.query.userId
-    db.query(`
-      SELECT user_id, games.id, games.name, games.cover
-      FROM games
-      JOIN user_game_relationships ON games.id = user_game_relationships.game_id
-      WHERE user_id = ${userId} AND user_game_relationships.liked = TRUE;
-    `).then((data => {
-      res.json(data.rows);
-      }))
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  })
-
-  router.get('/user/followers', (req, res) => {
-    const userId = req.query.userId
-    db.query(`
-      SELECT follower_id AS id, users.username
-      FROM followers
-      JOIN users ON followers.follower_id = users.id
-      WHERE user_id = ${userId};
-    `).then((data => {
-      res.json(data.rows);
-      }))
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  })
-  //route to add follower
-  router.post('/user/follow', (req, res) => {
-    const mainUserId = req.body.params.mainUserId;
-    const followUserId = req.body.params.followUserId;
-    db.query(`
-      INSERT INTO followers (user_id, follower_id) VALUES (${followUserId}, ${mainUserId})
-    `).then((data => {
-      res.json(data.rows);
-      }))
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  })
-
-  //route to delete follower
-
-  router.post('/user/unfollow', (req, res) => {
-    const mainUserId = req.body.params.mainUserId;
-    const followUserId = req.body.params.followUserId;
-    db.query(`
-      DELETE FROM followers WHERE user_id = ${followUserId} AND follower_id = ${mainUserId};
-    `).then((data => {
-      res.json(data.rows);
-      }))
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  })
-
+  
   return router  
 }
