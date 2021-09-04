2b83fd557e51fcef97ce7157cefaf39729a73bd8
Sylas Serné <its.sylas@gmail.com>
2021-09-04 11:33:43 -0700

    add screenshots to game page

--- react-front-end/src/components/Game.js
+++ react-front-end/src/components/Game.js
@@ -32,7 +32,26 @@ export default function Game(props) {
 
   return (
     <div className="main-container">
-    <Carousel fade interval={750}>
+      <div className="game-info">
+        <div className="cover">
+          <img className="cover-img" src={game.cover}/>
+        </div>
+        <div className="game-details">
+          <div className="top">
+            <h2 >{ game.name }</h2>
+            <h4>{date}</h4>
+          </div>
+            <span>{ game.summary }</span>
+          <div className="bottom">
+          </div>
+        </div>
+        <div className = "user-game-interactions">
+          <span>buttons to like/review/follow the GAME</span>
+            <Button className="btn"> submit review </Button> 
+        </div>
+      </div>
+      <div className="carousel">
+    <Carousel fade interval={1000} controls={false}>
     <Carousel.Item>
         <img
           className="slideshow-image"
@@ -52,24 +71,7 @@ export default function Game(props) {
         />
       </Carousel.Item>
     </Carousel>
-      <div className="game-info">
-        <div className="imgGallery">
-          <img className="gallery-img" src={game.cover}/>
-        </div>
-        <div className="game-details">
-          <div className="top">
-            <h2 >{ game.name }</h2>
-            <h4>{date}</h4>
-          </div>
-            <span>{ game.summary }</span>
-          <div className="bottom">
-          </div>
-        </div>
-        <div className = "user-game-interactions">
-          <span>buttons to like/review/follow the GAME</span>
-            <Button className="btn"> submit review </Button> 
-        </div>
-      </div>
+    </div>
       <div>
         {/* plug in reviews component */}
       </div>
