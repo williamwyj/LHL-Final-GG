8ff606d431432d6ca4d027c9d8b6c43a24347f1d
Sylas Serné <its.sylas@gmail.com>
2021-09-03 22:54:25 -0700

    implement initial Screenshots component

---
+++ react-front-end/src/components/GamePageComponents/Screenshots.js
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react';
+import { Carousel } from 'react-bootstrap';
+
+export default function Game() {
+  return(
+    <Carousel fade interval={750}>
+      <Carousel.Item>
+        <img
+          className=""
+          
+        />
+      </Carousel.Item>
+    </Carousel>
+  )
+}
