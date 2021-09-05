920ba05d19be000464e3d8ee968ec80317e70375
Sylas Serné <its.sylas@gmail.com>
2021-09-05 15:35:05 -0700

    further styling

--- react-front-end/src/components/Home.js
+++ react-front-end/src/components/Home.js
@@ -1,44 +1,51 @@
-import React from 'react';
-import axios from 'axios';
-import "./Home.scss"
+import React from "react";
+import axios from "axios";
+import "./Home.scss";
 
-import FeaturedGame from './GameBox/FeaturedGames';
-import TopUsers from "./HomePage/TopUsers"
+import FeaturedGame from "./GameBox/FeaturedGames";
+import TopUsers from "./HomePage/TopUsers";
 
-import useApplicationData from './hooks/useApplicationData';
-import TopReviews from './Reviews/TopReviews';
+import useApplicationData from "./hooks/useApplicationData";
+import TopReviews from "./Reviews/TopReviews";
 
 export default function Home() {
-
   const { state } = useApplicationData();
   // const fetchData = () => {
   //   axios.get('/api/games') // You can simply make your requests to "/api/whatever you want"
   //   .then((response) => {
   //     // handle success
   //     console.log(response.data) // The entire response from the Rails API
-  //   }) 
+  //   })
   // }
 
-  
   return (
     <div className="App">
-      <div className="homePageGamesTitle">
-        What's Hot This Week
+      <div className="home-intro-heading">
+        <div className="title-line-left">Good games are</div>
+        <div className="title-line-right">Trash</div>
+        <div className="title-line-right delayed"></div>
+        <div className="title-line-right delayed"></div>
       </div>
+      <div className="home-body">
+        <div className="homePageGamesTitle">What's Hot This Week</div>
         <FeaturedGame state={state} />
-      <div className="topReviewsUsers">
-        <TopReviews reviews={state.reviews}/>
-        
-        <ul className="topUsers">
-          {state.users.map((user) => {
-            return <TopUsers key={user.user_id} userId={user.user_id} username={user.username} followers={user.count} />
-          }
-          )}
-        </ul>  
+        <div className="topReviewsUsers">
+          <TopReviews reviews={state.reviews} />
+
+          <ul className="topUsers">
+            {state.users.map((user) => {
+              return (
+                <TopUsers
+                  userId={user.user_id}
+                  username={user.username}
+                  followers={user.count}
+                  thumbnail={user.thumbnail}
+                />
+              );
+            })}
+          </ul>
+        </div>
       </div>
-       
     </div>
   );
-  
 }
-
