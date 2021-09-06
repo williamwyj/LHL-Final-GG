d866dd4f54090190c02a4fecac5df83281377d3f
cytusalive <cytusomega@gmail.com>
2021-09-05 22:14:34 -0700

    showing user information with link to user page

--- react-front-end/src/components/HomePage/TopUsers.js
+++ react-front-end/src/components/HomePage/TopUsers.js
@@ -1,17 +1,28 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import Axios from "axios";
+import React, { useState } from "react"
 
 import "./TopUsers.scss"
 
 export default function TopUsers(props) {
+  const userLink = `/user/${props.userId}`;
+  const [reviews, setReviews] = useState(0);
+  Axios.get("/api/user/reviewStats", {
+    params: {
+      userId : props.userId
+    }
+  })
+  .then(res => {
+    setReviews(res.data[0].reviews);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
   return (
   <li className="homePageTopUsers">
-    User ID
-    {props.user_id}
-    User Name
-    {props.username}
-    Followers
-    {props.followers}
+    <a href={userLink}> {props.username}</a>
+    <div> Reviews: {reviews} </div>
+    <div> Followers: {props.followers} </div>
   </li>
   )
 }
