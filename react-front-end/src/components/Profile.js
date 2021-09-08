c98f1ec10de05f8d2dff5cf476bf90b901a72f67
Sylas Serné <its.sylas@gmail.com>
2021-09-07 19:46:31 -0700

    many tweaks to make styling of reviews uniform on all pages

--- react-front-end/src/components/Profile.js
+++ react-front-end/src/components/Profile.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import UserProfile from './ProfilePage/UserProfile'
-import FeaturedGame from './GameBox/FeaturedGames';
-import TopReviews from './Reviews/TopReviews'
+import UserProfile from "./ProfilePage/UserProfile";
+import FeaturedGame from "./GameBox/FeaturedGames";
+import TopReviews from "./Reviews/TopReviews";
 
-import useUserInfo from './hooks/useUserInfo';
+import useUserInfo from "./hooks/useUserInfo";
 
-import "./Profile.scss"
-import { useParams } from 'react-router-dom';
-
-
-
+import "./Profile.scss";
+import { useParams } from "react-router-dom";
 
 export default function Profile() {
   //get the id from url
   const { name } = useParams();
-  const {state} = useUserInfo(name);
+  const { state } = useUserInfo(name);
 
   return (
     <div>
-      <UserProfile thumbnail={state.thumbnail} id={state.id} username={state.username} followers={state.followers} followerNames={state.followerNames} followed={state.followed} reviews={state.reviews}/>
-      <FeaturedGame state={state} /> 
-      {state.topReviews && <TopReviews reviews={state.topReviews} />}
-      {!state.topReviews[0] && <p>You have not written any reviews! Write a review to feature on your profile page!</p>}
+      <UserProfile
+        thumbnail={state.thumbnail}
+        id={state.id}
+        username={state.username}
+        followers={state.followers}
+        followerNames={state.followerNames}
+        followed={state.followed}
+        reviews={state.reviews}
+      />
+      <FeaturedGame state={state} />
+      <div className="userPageReviews">
+        {state.topReviews && <TopReviews reviews={state.topReviews} />}
+        {!state.topReviews[0] && (
+          <p>
+            You have not written any reviews! Write a review to feature on your
+            profile page!
+          </p>
+        )}
+      </div>
     </div>
   );
 }
