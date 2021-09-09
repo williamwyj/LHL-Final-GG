87e6de21c57957cb1c15caccd310bc924483577f
Sylas Serné <its.sylas@gmail.com>
2021-09-09 09:44:49 -0700

    fixed display glitch

--- react-front-end/src/components/SplashPage/SplashHeader.js
+++ react-front-end/src/components/SplashPage/SplashHeader.js
@@ -5,14 +5,13 @@ export default function SplashHeader() {
   const [state, setState] = useState({
     class: "active",
     value: "talked about.",
-    counter: 0,
   });
 
   useEffect(() => {
     const initialState = {
       class: "active",
       value: "talked about.",
-      counter: 0,
+
     };
     if (state.class === "coming") {
       setTimeout(() => {
@@ -24,45 +23,35 @@ export default function SplashHeader() {
         setState((state) => ({ ...state, class: "going" }));
       }, 3000);
     }
-    if (state.class === "going") {
-      const count = state.counter + 1;
-      setTimeout(() => {
-        setState((state) => ({ ...state, class: "coming", counter: count }));
-      }, 2002);
-    }
-    if (state.class === "going" && state.counter === 0) {
+    if (state.class === "going" && state.value === "talked about.") {
       setTimeout(() => {
         setState((state) => ({
           class: "coming",
           value: "online.",
-          counter: 1
         }));
       }, 2002);
     }
-    if (state.class === "going" && state.counter === 1) {
+    if (state.class === "going" && state.value === "online.") {
       setTimeout(() => {
         setState((state) => ({
           class: "coming",
           value: "intriguing.",
-          counter: 2
         }));
       }, 2002);
     }
-    if (state.class === "going" && state.counter === 2) {
+    if (state.class === "going" && state.value === "intriguing.") {
       setTimeout(() => {
         setState((state) => ({
           class: "coming",
           value: "here.",
-          counter: 3
         }));
       }, 2002);
     }
-    if (state.class === "going" && state.counter === 3) {
+    if (state.class === "going" && state.value === "here.") {
       setTimeout(() => {
         setState((state) => ({
           class: "coming",
           value: "talked about.",
-          counter: 0
         }));
       }, 2002);
     }
